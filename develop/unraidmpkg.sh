5b67b717631d87545a1e63a53fedbbbbd391a24a
jsavargas <jsavargas@gmail.com>
2023-02-27 21:13:55 -0300

    testing dev

--- develop/unraidmpkg.sh
+++ develop/unraidmpkg.sh
@@ -17,6 +17,6 @@ echo "${BASH_SOURCE[0]}"
 md5=($(md5sum ${BASEDIR}/../archive/NerdTools-Dev-${version}-x86_64-1.txz))
 echo ${md5}
 
-sed -i "s/ENTITY version   .*/ENTITY version   \"$version\">"/ "$BASEDIR/../plugin/NerdTools.plg"
-sed -i "s/ENTITY md5       .*/ENTITY md5       \"$md5\">"/ "$BASEDIR/../plugin/NerdTools.plg"
+sed -i "s/ENTITY version   .*/ENTITY version   \"$version\">"/ "$BASEDIR/../plugin/NerdTools-Dev.plg"
+sed -i "s/ENTITY md5       .*/ENTITY md5       \"$md5\">"/ "$BASEDIR/../plugin/NerdTools-Dev.plg"
 
